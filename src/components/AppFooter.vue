8e5eb5500356b50ae4856f3226f0f6a5bb5175d0
DelishThai <info@delishthai.nl>
2025-11-19 12:40:52 +0100

    change opening time

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -19,9 +19,8 @@ import InstagramIcon from '@/components/icons/social/InstagramIcon.vue'
             class="flex flex-col sm:flex-row justify-between gap-4 text-sm text-gray-secondary font-medium px-[6px]"
           >
             <div class="flex flex-col gap-2 items-start">
-              <span> Din - do: 12:00-19:00 </span>
-              <span> Vrijdag: 12:00-20:00 </span>
-              <span> Zaterdag: 12:00-19:00 </span>
+              <span> Din - do: 16:00-19:00 </span>
+              <span> Vrijdag - zaterdag: 12:00-20:00 </span>
 
               <span> Bernard van Kreelpoort 5, Veenendaal </span>
             </div>
